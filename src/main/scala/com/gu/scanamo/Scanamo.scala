c248fb0272831eaab295fd8917cd210ea2626489
Philip Wills <philip.wills@guardian.co.uk>
2016-02-29 18:56:51 +0000

    Add support for scanning a table

--- src/main/scala/com/gu/scanamo/Scanamo.scala
+++ src/main/scala/com/gu/scanamo/Scanamo.scala
@@ -1,9 +1,11 @@
 package com.gu.scanamo
 
-import cats.data.ValidatedNel
+import cats.Later
+import cats.data.{Streaming, ValidatedNel}
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
 import com.amazonaws.services.dynamodbv2.model._
-import collection.convert.decorateAsJava._
+
+import collection.convert.decorateAll._
 
 /**
   * Scanamo provides a simplified interface for reading and writing case classes to DynamoDB
@@ -41,7 +43,7 @@ object Scanamo {
     */
   def get[K, T](client: AmazonDynamoDB)(tableName: String)(key: (String, K)*)
     (implicit fk: DynamoFormat[K], ft: DynamoFormat[T]): Option[ValidatedNel[DynamoReadError, T]] =
-    from(client.getItem(getRequest(tableName)(key: _*)))
+    Option(client.getItem(getRequest(tableName)(key: _*)).getItem).map(read[T])
 
   /**
     * {{{
@@ -57,6 +59,34 @@ object Scanamo {
   def delete[K, T](client: AmazonDynamoDB)(tableName: String)(key: (String, K)*)
     (implicit fk: DynamoFormat[K], ft: DynamoFormat[T]): DeleteItemResult =
     client.deleteItem(deleteRequest(tableName)(key: _*))
+
+  /**
+    * Lazily scans a DynamoDB table
+    *
+    * Does not cache results by default
+    * {{{
+    * >>> val client = LocalDynamoDB.client()
+    * >>> import com.amazonaws.services.dynamodbv2.model._
+    * >>> val createTableResult = LocalDynamoDB.createTable(client, "bears", List("name" -> ScalarAttributeType.S), List("name" -> KeyType.HASH))
+    * >>>
+    * >>> case class Bear(name: String, favouriteFood: String)
+    * >>> val r1 = Scanamo.put(client)("bears")(Bear("Pooh", "honey"))
+    * >>> val r2 = Scanamo.put(client)("bears")(Bear("Yogi", "picnic baskets"))
+    * >>> Scanamo.scan[Bear](client)("bears").toList
+    * List(Valid(Bear(Pooh,honey)), Valid(Bear(Yogi,picnic baskets)))
+    * }}}
+    */
+  def scan[T](client: AmazonDynamoDB)(tableName: String)(implicit f: DynamoFormat[T]): Streaming[ValidatedNel[DynamoReadError, T]] = {
+    def scanMore(lastKey: Option[java.util.Map[String, AttributeValue]]): Streaming[ValidatedNel[DynamoReadError, T]] = {
+      val tableRequest = new ScanRequest().withTableName(tableName)
+      val scanResult = client.scan(lastKey.map(k => tableRequest.withExclusiveStartKey(k)).getOrElse(tableRequest))
+      val items = Streaming.fromIterable(scanResult.getItems.asScala.map(read[T]))
+      Option(scanResult.getLastEvaluatedKey).map { key =>
+        items ++ Later(scanMore(Some(key)))
+      }.getOrElse(items)
+    }
+    scanMore(None)
+  }
 
   /**
     * {{{
@@ -99,11 +129,11 @@ object Scanamo {
     * prop> import collection.convert.decorateAsJava._
     * prop> import com.amazonaws.services.dynamodbv2.model._
     * prop> (m: Map[String, Int]) =>
-    *     |   Scanamo.from[Map[String, Int]](
-    *     |     new GetItemResult().withItem(m.mapValues(i => new AttributeValue().withN(i.toString)).asJava)
-    *     |   ) == Some(cats.data.Validated.valid(m))
+    *     |   Scanamo.read[Map[String, Int]](
+    *     |     m.mapValues(i => new AttributeValue().withN(i.toString)).asJava
+    *     |   ) == cats.data.Validated.valid(m)
     * }}}
     */
-  def from[T](result: GetItemResult)(implicit f: DynamoFormat[T]): Option[ValidatedNel[DynamoReadError, T]] =
-    Option(result.getItem).map(i => f.read(new AttributeValue().withM(i)))
+  def read[T](m: java.util.Map[String, AttributeValue])(implicit f: DynamoFormat[T]): ValidatedNel[DynamoReadError, T] =
+    f.read(new AttributeValue().withM(m))
 }
