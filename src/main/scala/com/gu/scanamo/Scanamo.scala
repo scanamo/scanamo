694c090119cb17fba3760f5e78c9550d8e5ba8a7
Philip Wills <philip.wills@guardian.co.uk>
2016-03-20 22:19:38 +0000

    Fixup table creation

--- src/main/scala/com/gu/scanamo/Scanamo.scala
+++ src/main/scala/com/gu/scanamo/Scanamo.scala
@@ -63,10 +63,8 @@ object Scanamo {
     * >>> Scanamo.get[Farmer](client)("farmers")('name === "Maggot")
     * Some(Valid(Farmer(Maggot,75,Farm(List(dog)))))
     *
-    * >>> import com.amazonaws.services.dynamodbv2.model._
-    * >>> val createTableResult = LocalDynamoDB.createTable(client, "engines",
-    * ... List("name" -> ScalarAttributeType.S, "number" -> ScalarAttributeType.N),
-    * ... List("name" -> KeyType.HASH, "number" -> KeyType.RANGE))
+    * >>> import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType._
+    * >>> val createTableResult = LocalDynamoDB.createTable(client)("engines")('name -> S, 'number -> N)
     * >>> case class Engine(name: String, number: Int)
     * >>> val thomas = Scanamo.put(client)("engines")(Engine("Thomas", 1))
     * >>> Scanamo.get[Engine](client)("engines")('name === "Thomas" and 'number === 1)
