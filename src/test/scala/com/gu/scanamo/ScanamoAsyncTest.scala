c0e8ac16928a702c481b0db8a96a5fbed02193a4
Susie Coleman <susie.coleman@guardian.co.uk>
2017-02-22 11:11:14 +0000

    Test put with get.

--- src/test/scala/com/gu/scanamo/ScanamoAsyncTest.scala
+++ src/test/scala/com/gu/scanamo/ScanamoAsyncTest.scala
@@ -21,8 +21,8 @@ class ScanamoAsyncTest extends FunSpec with Matchers with ScalaFutures {
       import com.gu.scanamo.syntax._
 
       val result = for {
-        item <- ScanamoAsync.put(client)("asyncFarmers")(Farmer("McDonald", 156L, Farm(List("sheep", "cow"))))
-      } yield item
+        _ <- ScanamoAsync.put(client)("asyncFarmers")(Farmer("McDonald", 156L, Farm(List("sheep", "cow"))))
+      } yield Scanamo.get[Farmer](client)("asyncFarmers")('name -> "McDonald")
 
       result.futureValue should equal(Farmer("McDonald", 156, Farm(List("sheep", "cow"))))
     }
