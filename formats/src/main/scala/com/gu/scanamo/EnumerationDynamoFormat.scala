708d309b7c331aa3b7debf299637e2ff867795e8
Regis Kuckaertz <regis_kuckaertz@33020.gnm.int>
2018-05-13 21:58:55 +0100

    Derive enums

--- formats/src/main/scala/com/gu/scanamo/EnumerationDynamoFormat.scala
+++ formats/src/main/scala/com/gu/scanamo/EnumerationDynamoFormat.scala
@@ -1,9 +1,10 @@
 package com.gu.scanamo
 
 import com.amazonaws.services.dynamodbv2.model.AttributeValue
-import com.gu.scanamo.error.{DynamoReadError, TypeCoercionError}
-import shapeless.labelled.{FieldType, field}
-import shapeless.{:+:, CNil, Coproduct, HNil, Inl, Inr, LabelledGeneric, Witness}
+import com.gu.scanamo.error._
+import cats.syntax.either._
+import magnolia._
+import scala.language.experimental.macros
 
 abstract class EnumerationDynamoFormat[T] extends DynamoFormat[T]
 
@@ -26,35 +27,21 @@ abstract class EnumerationDynamoFormat[T] extends DynamoFormat[T]
   * Zebra
   * }}}
   */
-trait EnumDynamoFormat extends DerivedDynamoFormat {
-  implicit val enumDynamoFormatCNil: EnumerationDynamoFormat[CNil] = new EnumerationDynamoFormat[CNil] {
-    override def read(av: AttributeValue): Either[DynamoReadError, CNil] = Left(
-      TypeCoercionError(new Exception(s"$av is not a recognised member of the Enumeration"))
-    )
-    override def write(t: CNil): AttributeValue = sys.error("Cannot write CNil")
-  }
+object EnumerationDynamoFormat {
+  def dispatch[T](st: SealedTrait[EnumerationDynamoFormat, T]): EnumerationDynamoFormat[T] =
+    new EnumerationDynamoFormat[T] {
+      def read(av: AttributeValue): Either[DynamoReadError, T] = 
+        Either.fromOption(
+          for {
+            typ <- Option(av.getS)
+            sub <- st.subtypes.find(_.typeName.short == typ)
+          } yield sub,
+          NoPropertyOfType("S", av)
+        ).flatMap(_.typeclass.read(av))
 
-  implicit def enumDynamoFormatCCons[K <: Symbol, V, R <: Coproduct](implicit
-                                                                     fieldWitness: Witness.Aux[K],
-                                                                     emptyGeneric: LabelledGeneric.Aux[V, HNil],
-                                                                     alternativeFormat: EnumerationDynamoFormat[R]
-                                                                    ): EnumerationDynamoFormat[FieldType[K, V] :+: R] = new EnumerationDynamoFormat[FieldType[K, V] :+: R] {
-    override def read(av: AttributeValue): Either[DynamoReadError, FieldType[K, V] :+: R] =
-      if (av.getS == fieldWitness.value.name) Right(Inl(field[K](emptyGeneric.from(HNil))))
-      else alternativeFormat.read(av).right.map(Inr(_))
-
-    override def write(t: FieldType[K, V] :+: R): AttributeValue = t match {
-      case Inl(_) => new AttributeValue().withS(fieldWitness.value.name)
-      case Inr(r) => alternativeFormat.write(r)
+      def write(t: T): AttributeValue =
+        st.dispatch(t) { sub => sub.typeclass.write(sub.cast(t)) }
     }
-  }
-
-  implicit def enumFormat[A, Repr <: Coproduct](implicit
-                                                gen: LabelledGeneric.Aux[A, Repr],
-                                                genericFormat: EnumerationDynamoFormat[Repr]
-                                               ): EnumerationDynamoFormat[A] =
-    new EnumerationDynamoFormat[A] {
-      override def read(av: AttributeValue): Either[DynamoReadError, A] = genericFormat.read(av).right.map(gen.from)
-      override def write(t: A): AttributeValue = genericFormat.write(gen.to(t))
-    }
+  
+  implicit def gen[T]: EnumerationDynamoFormat[T] = macro Magnolia.gen[T]
 }
