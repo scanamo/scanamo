4367b53369df928dd5658cda5885f8d14dbd8822
Regis Kuckaertz <regis_kuckaertz@33020.gnm.int>
2018-05-14 07:16:39 +0100

    Move functions out of object

--- formats/src/main/scala/com/gu/scanamo/DerivedDynamoFormat.scala
+++ formats/src/main/scala/com/gu/scanamo/DerivedDynamoFormat.scala
@@ -16,25 +16,27 @@ trait DerivedDynamoFormat {
 
   type Valid[A] = ValidatedNel[PropertyReadError, A]
 
-  def combine[T](cc: CaseClass[DynamoFormat, T]): DynamoFormat[T] = new DynamoFormat[T] {
-    private final def decodeField[A](m: Map[String, AttributeValue])(p: Param[DynamoFormat, A]): Valid[p.PType] =
+  def combine[T](cc: CaseClass[DynamoFormat, T]): DynamoFormat[T] = {
+    def decodeField[A](m: Map[String, AttributeValue])(p: Param[DynamoFormat, A]): Valid[p.PType] =
       Either.fromOption(m.get(p.label), PropertyReadError(p.label, MissingProperty))
         .flatMap(v => p.typeclass.read(v).leftMap(PropertyReadError(p.label, _)))
         .toValidatedNel
 
-    private final def decode(av: AttributeValue): Valid[Seq[Any]] =
+    def decode(av: AttributeValue): Valid[Seq[Any]] =
       Option(av.getM).map(_.asScala.toMap) match {
         case Some(m) => cc.parameters.toList.traverse(decodeField(m)(_))
         case None => PropertyReadError(cc.typeName.full, NoPropertyOfType("M", av)).invalidNel
       }
 
-    def read(av: AttributeValue): Either[DynamoReadError, T] = 
-      decode(av).fold(fe => Left(InvalidPropertiesError(fe)), fa => Right(cc.rawConstruct(fa)))
+    new DynamoFormat[T] {
+      def read(av: AttributeValue): Either[DynamoReadError, T] = 
+        decode(av).fold(fe => Left(InvalidPropertiesError(fe)), fa => Right(cc.rawConstruct(fa)))
 
-    def write(t: T): AttributeValue =
-      new AttributeValue().withM(cc.parameters.map { p => 
-        p.label -> p.typeclass.write(p.dereference(t)) 
-      }.toMap.asJava)
+      def write(t: T): AttributeValue =
+        new AttributeValue().withM(cc.parameters.map { p => 
+          p.label -> p.typeclass.write(p.dereference(t)) 
+        }.toMap.asJava)
+    }
   }
 
   def dispatch[T](st: SealedTrait[DynamoFormat, T]): DynamoFormat[T] =
