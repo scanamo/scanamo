4470f29656ec185e0014ddbc2329d1a62987313c
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-07-22 14:40:28 +0100

    Revert "Apply in request"
    This reverts commit 475ca1d4386225c440ba59f986f8a134a06e8a11.

--- scanamo/src/main/scala/com/gu/scanamo/ops/ScanamoInterpreters.scala
+++ scanamo/src/main/scala/com/gu/scanamo/ops/ScanamoInterpreters.scala
@@ -170,10 +170,7 @@ private[ops] object JavaRequests {
         })
       )
       .reduceLeft(_.compose(_))(
-        req.query(new QueryRequest()
-          .withTableName(req.tableName)
-          .withConsistentRead(req.options.consistent))
-          .withScanIndexForward(req.options.scanIndexForward)
+        req.query(new QueryRequest().withTableName(req.tableName).withConsistentRead(req.options.consistent))
       )
   }
 
