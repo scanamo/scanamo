33585391f5a87ee20d84a04b27880512629aecb1
TiwaryAyush <ayush.tiwari@knoldus.in>
2019-05-11 20:12:28 +0530

    formated the codebase

--- alpakka/src/main/scala/org/scanamo/ScanamoAlpakka.scala
+++ alpakka/src/main/scala/org/scanamo/ScanamoAlpakka.scala
@@ -3,9 +3,9 @@ package org.scanamo
 import akka.stream.alpakka.dynamodb.AwsOp
 import akka.stream.alpakka.dynamodb.scaladsl.DynamoClient
 import org.scanamo.ops.retrypolicy.RetryPolicy
-import org.scanamo.ops.{ AlpakkaInterpreter, ScanamoOps }
+import org.scanamo.ops.{AlpakkaInterpreter, ScanamoOps}
 
-import scala.concurrent.{ ExecutionContext, Future }
+import scala.concurrent.{ExecutionContext, Future}
 
 /**
   * Provides the same interface as [[org.scanamo.Scanamo]], except that it requires an
@@ -15,9 +15,11 @@ import scala.concurrent.{ ExecutionContext, Future }
 object ScanamoAlpakka {
   import cats.instances.future._
 
-  def exec[A](client: DynamoClient)(op: ScanamoOps[A])(implicit ec: ExecutionContext, retrySettings : RetryPolicy): Future[A] =
+  def exec[A](client: DynamoClient)(op: ScanamoOps[A])(implicit ec: ExecutionContext,
+                                                       retrySettings: RetryPolicy): Future[A] =
     op.foldMap(AlpakkaInterpreter.future(client, retrySettings)(ec))
 
-  def singleRequest(client: DynamoClient, op: AwsOp)(implicit ec: ExecutionContext, retrySettings: RetryPolicy) : Future[AwsOp#B] =
+  def singleRequest(client: DynamoClient, op: AwsOp)(implicit ec: ExecutionContext,
+                                                     retrySettings: RetryPolicy): Future[AwsOp#B] =
     AlpakkaInterpreter.executeSingleRequest(client, op, retrySettings)(ec)
 }
