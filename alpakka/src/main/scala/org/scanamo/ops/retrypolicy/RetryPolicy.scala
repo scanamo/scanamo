651aa6c1838df70142768cc6490d3e1f18246a60
Regis Kuckaertz <regis.kuckaertz@guardian.co.uk>
2019-05-17 16:56:56 +0100

    Drop scheduler

--- alpakka/src/main/scala/org/scanamo/ops/retrypolicy/RetryPolicy.scala
+++ alpakka/src/main/scala/org/scanamo/ops/retrypolicy/RetryPolicy.scala
@@ -1,21 +1,10 @@
 package org.scanamo.ops.retrypolicy
 
-import java.util.concurrent.ScheduledExecutorService
-
 import org.scanamo.ops.retrypolicy.RetryPolicy._
 
 import scala.concurrent.duration.FiniteDuration
 
 sealed abstract class RetryPolicy extends Product with Serializable { self =>
-  final def maybeScheduler: Option[ScheduledExecutorService] = self match {
-    case policy @ Constant(_)        => Some(policy.scheduler)
-    case policy @ Linear(_, _)       => Some(policy.scheduler)
-    case policy @ Exponential(_, _)  => Some(policy.scheduler)
-    case And(thisPolicy, thatPolicy) => thisPolicy.maybeScheduler orElse thatPolicy.maybeScheduler
-    case Or(thisPolicy, thatPolicy)  => thisPolicy.maybeScheduler orElse thatPolicy.maybeScheduler
-    case _                           => None
-  }
-
   final def done: Boolean = self match {
     case Max(1)                      => true
     case And(thisPolicy, thatPolicy) => thisPolicy.done && thatPolicy.done
@@ -36,9 +25,9 @@ sealed abstract class RetryPolicy extends Product with Serializable { self =>
   }
 
   final def update: RetryPolicy = self match {
-    case policy: Constant[_]         => policy
-    case policy: Linear[_]           => policy
-    case policy: Exponential[_]      => policy
+    case policy: Constant         => policy
+    case policy: Linear           => policy
+    case policy: Exponential      => policy
     case Max(retries)                => Max(retries - 1)
     case And(thisPolicy, thatPolicy) => And(thisPolicy.update, thatPolicy.update)
     case Or(thisPolicy, thatPolicy)  => Or(thisPolicy.update, thatPolicy.update)
@@ -57,22 +46,9 @@ sealed abstract class RetryPolicy extends Product with Serializable { self =>
 }
 
 object RetryPolicy {
-  final case class Constant[S <: ScheduledExecutorService](retryDelay: FiniteDuration)(
-    implicit val scheduler: S
-  ) extends RetryPolicy
-
-  final case class Linear[S <: ScheduledExecutorService](
-    retryDelay: FiniteDuration,
-    factor: Double
-  )(implicit val scheduler: S)
-      extends RetryPolicy
-
-  final case class Exponential[S <: ScheduledExecutorService](
-    retryDelay: FiniteDuration,
-    factor: Double
-  )(implicit val scheduler: S)
-      extends RetryPolicy
-
+  final case class Constant(retryDelay: FiniteDuration) extends RetryPolicy
+  final case class Linear(retryDelay: FiniteDuration, factor: Double) extends RetryPolicy
+  final case class Exponential(retryDelay: FiniteDuration, factor: Double) extends RetryPolicy
   final case class Max(numberOfRetries: Int) extends RetryPolicy
   final case class And(thisPolicy: RetryPolicy, thatPolicy: RetryPolicy) extends RetryPolicy
   final case class Or(thisPolicy: RetryPolicy, thatPolicy: RetryPolicy) extends RetryPolicy
