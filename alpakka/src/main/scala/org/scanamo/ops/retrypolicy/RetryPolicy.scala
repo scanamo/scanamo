0271920d99e192cb588b9e2c0d2183499a7a8023
Regis Kuckaertz <regis.kuckaertz@guardian.co.uk>
2019-06-27 13:54:04 +0100

    Fix RetryPolicy

--- alpakka/src/main/scala/org/scanamo/ops/retrypolicy/RetryPolicy.scala
+++ alpakka/src/main/scala/org/scanamo/ops/retrypolicy/RetryPolicy.scala
@@ -5,30 +5,29 @@ import org.scanamo.ops.retrypolicy.RetryPolicy._
 import scala.concurrent.duration.FiniteDuration
 
 sealed abstract class RetryPolicy extends Product with Serializable { self =>
-  final def done: Boolean = self match {
-    case Max(1)                      => true
-    case And(thisPolicy, thatPolicy) => thisPolicy.done && thatPolicy.done
-    case Or(thisPolicy, thatPolicy)  => thisPolicy.done || thatPolicy.done
-    case Never                       => true
-    case _                           => false
+  final def continue: Boolean = self match {
+    case Max(0) | Never              => false
+    case And(thisPolicy, thatPolicy) => thisPolicy.continue && thatPolicy.continue
+    case Or(thisPolicy, thatPolicy)  => thisPolicy.continue || thatPolicy.continue
+    case _                           => true
   }
 
   final def delay: Long = self match {
-    case Constant(retryDelay)       => retryDelay.toMillis
-    case Linear(retryDelay, factor) => retryDelay.toMillis * factor.toLong
-    case Exponential(retryDelay, factor) =>
-      val delayMillis = retryDelay.toMillis
-      delayMillis * Math.pow(delayMillis.toDouble, factor).toLong
-    case And(thisPolicy, thatPolicy) => Math.max(thisPolicy.delay, thatPolicy.delay)
-    case Or(thisPolicy, thatPolicy)  => Math.min(thisPolicy.delay, thatPolicy.delay)
-    case _                           => 0
+    case Constant(retryDelay)            => retryDelay.toMillis
+    case Linear(retryDelay, factor)      => retryDelay.toMillis * factor.toLong
+    case Exponential(retryDelay, factor) => retryDelay.toMillis * factor.toLong
+    case And(thisPolicy, thatPolicy)     => Math.max(thisPolicy.delay, thatPolicy.delay)
+    case Or(thisPolicy, thatPolicy)      => Math.min(thisPolicy.delay, thatPolicy.delay)
+    case _                               => 0
   }
 
   final def update: RetryPolicy = self match {
-    case Max(retries)                => Max(retries - 1)
-    case And(thisPolicy, thatPolicy) => And(thisPolicy.update, thatPolicy.update)
-    case Or(thisPolicy, thatPolicy)  => Or(thisPolicy.update, thatPolicy.update)
-    case retryPolicy                 => retryPolicy
+    case Max(retries) if retries > 0     => Max(retries - 1)
+    case And(thisPolicy, thatPolicy)     => And(thisPolicy.update, thatPolicy.update)
+    case Or(thisPolicy, thatPolicy)      => Or(thisPolicy.update, thatPolicy.update)
+    case Linear(retryDelay, factor)      => Linear(retryDelay, factor + 1)
+    case Exponential(retryDelay, factor) => Exponential(retryDelay, factor * 2)
+    case retryPolicy                     => retryPolicy
   }
 
   final val retries: Int = self match {
