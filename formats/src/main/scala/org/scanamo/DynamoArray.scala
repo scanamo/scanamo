715d4475f388b0f167a6845246aa6ecbe94a08de
Sam Desborough <sam.desborough@gmail.com>
2019-07-11 12:09:02 +0100

    Create static empty list attribute in DynamoArray

--- formats/src/main/scala/org/scanamo/DynamoArray.scala
+++ formats/src/main/scala/org/scanamo/DynamoArray.scala
@@ -83,7 +83,7 @@ sealed abstract class DynamoArray extends Product with Serializable { self =>
     */
   // TODO mark as private?
   final def toAttributeValue: AttributeValue = self match {
-    case Empty       => new AttributeValue().withL()
+    case Empty       => emptyListAttributeValue
     case Strict(xs)  => new AttributeValue().withL(xs)
     case StrictS(xs) => new AttributeValue().withSS(xs)
     case StrictN(xs) => new AttributeValue().withNS(xs)
@@ -315,4 +315,9 @@ object DynamoArray {
     * The empty array
     */
   val empty: DynamoArray = Empty
+
+  /**
+    * An AttributeValue representing an empty list
+    */
+  private val emptyListAttributeValue: AttributeValue = new AttributeValue().withL()
 }
