98493403ebf3ae8503b2911ebbef818d29bd7973
Regis Kuckaertz <regis.kuckaertz@guardian.co.uk>
2019-09-19 11:45:18 +0100

    Fix joda

--- joda/src/test/scala/org/scanamo/joda/TimeGenerators.scala
+++ joda/src/test/scala/org/scanamo/joda/TimeGenerators.scala
@@ -7,14 +7,21 @@ import Arbitrary.arbitrary
 object TimeGenerators {
   implicit val dateTimeArb: Arbitrary[DateTime] = Arbitrary {
     for {
-      year <- Gen.choose[Int](Years.MIN_VALUE.getYears, Years.MAX_VALUE.getYears)
+      year <- Gen.choose[Int](-292275055, 292278994)
       month <- Gen.choose[Int](1, 12)
-      days = new YearMonth(year, month).monthOfYear.getMaximumValue
+      days = new DateTime(year, month, 1, 0, 0, 0, 0).dayOfMonth.getMaximumValue
       day <- Gen.choose[Int](1, days)
       hour <- Gen.choose[Int](0, 23)
       minute <- Gen.choose[Int](0, 59)
       second <- Gen.choose[Int](0, 59)
       milli <- Gen.choose[Int](0, 999)
-    } yield new DateTime(year, month, day, hour, minute, second, milli * 1000000, DateTimeZone.UTC)
+    } yield new DateTime(year, month, day, hour, minute, second, milli, DateTimeZone.UTC)
   }
+
+  implicit val instantAsLongArb: Arbitrary[Instant] = Arbitrary {
+    for {
+      l <- arbitrary[Long]
+    } yield Instant.ofEpochMilli(l)
+  }
+
 }
