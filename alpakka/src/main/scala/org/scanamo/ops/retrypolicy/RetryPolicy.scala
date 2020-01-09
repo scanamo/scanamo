9c09b972bc270ffbbb2d0dc890a169968fcf220d
Divyanshu564 <divyanshu.srivastava@knoldus.com>
2020-01-09 18:15:32 +0530

    retry policy updated

--- alpakka/src/main/scala/org/scanamo/ops/retrypolicy/RetryPolicy.scala
+++ alpakka/src/main/scala/org/scanamo/ops/retrypolicy/RetryPolicy.scala
@@ -16,11 +16,9 @@
 
 package org.scanamo.ops.retrypolicy
 
+import java.util.concurrent.ThreadLocalRandom
 import org.scanamo.ops.retrypolicy.RetryPolicy._
-
 import scala.concurrent.duration.Duration
-
-import scala.util.Random
 
 sealed abstract class RetryPolicy extends Product with Serializable { self =>
   final def continue: Boolean = self match {
@@ -29,45 +27,47 @@ sealed abstract class RetryPolicy extends Product with Serializable { self =>
     case Or(thisPolicy, thatPolicy)  => thisPolicy.continue || thatPolicy.continue
     case _                           => true
   }
-  
+
   final def delay: Long = self match {
-    case Constant(retryDelay) =>
+    case Constant(retryDelay, min, max) =>
       val currentRetryDelay = retryDelay.toMillis
-      getDelayWithJitter(currentRetryDelay)
-    case Linear(retryDelay, factor) =>
+      getDelayWithJitter(currentRetryDelay, min, max)
+    case Linear(retryDelay, factor, min, max) =>
       val currentRetryDelay = retryDelay.toMillis * factor.toLong
-      getDelayWithJitter(currentRetryDelay)
-    case Exponential(retryDelay, factor, _) =>
+      getDelayWithJitter(currentRetryDelay, min, max)
+    case Exponential(retryDelay, factor, min, max, _) =>
       val delayMillis = retryDelay.toMillis
       val currentRetryDelay = delayMillis * Math.pow(delayMillis.toDouble, factor).toLong
-      getDelayWithJitter(currentRetryDelay)
+      getDelayWithJitter(currentRetryDelay, min, max)
     case And(thisPolicy, thatPolicy) => Math.max(thisPolicy.delay, thatPolicy.delay)
     case Or(thisPolicy, thatPolicy)  => Math.min(thisPolicy.delay, thatPolicy.delay)
     case _                           => 0
   }
 
   final def update: RetryPolicy = self match {
-    case Max(retries) if retries > 0        => Max(retries - 1)
-    case And(thisPolicy, thatPolicy)        => And(thisPolicy.update, thatPolicy.update)
-    case Or(thisPolicy, thatPolicy)         => Or(thisPolicy.update, thatPolicy.update)
-    case Linear(retryDelay, n)              => Linear(retryDelay, n + 1)
-    case Exponential(retryDelay, factor, n) => Exponential(retryDelay, factor, n + 1)
-    case retryPolicy                        => retryPolicy
+    case Max(retries) if retries > 0                  => Max(retries - 1)
+    case And(thisPolicy, thatPolicy)                  => And(thisPolicy.update, thatPolicy.update)
+    case Or(thisPolicy, thatPolicy)                   => Or(thisPolicy.update, thatPolicy.update)
+    case Linear(retryDelay, n, min, max)              => Linear(retryDelay, n + 1, min, max)
+    case Exponential(retryDelay, factor, n, min, max) => Exponential(retryDelay, factor, n + 1, min, max)
+    case retryPolicy                                  => retryPolicy
   }
 
   final def &&(that: RetryPolicy): RetryPolicy = And(self, that)
   final def ||(that: RetryPolicy): RetryPolicy = Or(self, that)
 
-  final private def getDelayWithJitter(delayInMillis: Long) = {
-    val maxJitter = Math.ceil(delayInMillis * 0.2).toInt
-    delayInMillis + Random.nextInt(maxJitter)
+  final private def getDelayWithJitter(delayInMillis: Long, lowerBound: Long, upperBound: Long) = {
+    val start = delayInMillis - lowerBound
+    val finish = delayInMillis + upperBound + 1
+    ThreadLocalRandom.current().nextLong(start, finish)
   }
 }
 
 object RetryPolicy {
-  final private case class Constant(retryDelay: Duration) extends RetryPolicy
-  final private case class Linear(retryDelay: Duration, n: Int) extends RetryPolicy
-  final private case class Exponential(retryDelay: Duration, factor: Double, n: Int) extends RetryPolicy
+  final private case class Constant(retryDelay: Duration, min: Long, max: Long) extends RetryPolicy
+  final private case class Linear(retryDelay: Duration, n: Int, min: Long, max: Long) extends RetryPolicy
+  final private case class Exponential(retryDelay: Duration, factor: Double, min: Long, max: Long, n: Int)
+      extends RetryPolicy
   final private case class Max(numberOfRetries: Int) extends RetryPolicy
   final private case class And(thisPolicy: RetryPolicy, thatPolicy: RetryPolicy) extends RetryPolicy
   final private case class Or(thisPolicy: RetryPolicy, thatPolicy: RetryPolicy) extends RetryPolicy
@@ -87,20 +87,20 @@ object RetryPolicy {
   def max(n: Int): RetryPolicy = if (n <= 0) never else Max(n)
 
   /** The policy that always retries, waiting `base` between each attempt */
-  def fixed(base: Duration): RetryPolicy =
+  def fixed(base: Duration, min: Long = 0, max: Long = 0): RetryPolicy =
     if (base < Duration.Zero) never
     else if (base == Duration.Zero) always
-    else Constant(base)
+    else Constant(base, min, max)
 
   /** The policy that always retries, waiting `base * n` where `n` is the number of attempts */
-  def linear(base: Duration): RetryPolicy =
+  def linear(base: Duration, min: Long = 0, max: Long = 0): RetryPolicy =
     if (base < Duration.Zero) never
     else if (base == Duration.Zero) always
-    else Linear(base, 1)
+    else Linear(base, 1, min, max)
 
   /** The policy that always retries, waiting `base * factor ^ n` where `n` is the number of attempts */
-  def exponential(base: Duration, factor: Double = 2): RetryPolicy =
+  def exponential(base: Duration, factor: Double = 2, min: Long = 0, max: Long = 0): RetryPolicy =
     if (base < Duration.Zero) never
     else if (base == Duration.Zero) always
-    else Exponential(base, factor, 0)
+    else Exponential(base, factor, min, max, 0)
 }
