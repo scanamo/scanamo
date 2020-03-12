640df1913c79f39266dc45d79e8bbfeefc7b9c4c
YaelMargalit <yaelm@carbyne911.com>
2020-03-12 17:25:40 +0200

    Add retry for AmazonDynamoDBException InternalFailure

--- alpakka/src/main/scala/org/scanamo/ScanamoAlpakka.scala
+++ alpakka/src/main/scala/org/scanamo/ScanamoAlpakka.scala
@@ -72,7 +72,7 @@ object ScanamoAlpakka extends AlpakkaInstances {
     case _: InternalServerErrorException | _: ItemCollectionSizeLimitExceededException | _: LimitExceededException |
         _: ProvisionedThroughputExceededException | _: RequestLimitExceededException =>
       true
-    case e: AmazonDynamoDBException if e.getErrorCode.contains("ThrottlingException") => true
+    case e: AmazonDynamoDBException if (e.getErrorCode.contains("ThrottlingException") | e.getErrorCode.contains("InternalFailure")) => true
     case _                                                                            => false
   }
 }
