813a1507449fe6cbe2e6cbe7a67dcac2aaa2470d
i10416 <ito.yo16uh90616@gmail.com>
2022-09-13 11:29:14 +0900

    chore: move src from scala-2.x to scala-2
    sbt uses src/main/scala-2 as an unmanaged src directory for cross build

--- scanamo/src/main/scala-2.x/org/scanamo/VersionSpecificFormats.scala
+++
@@ -1,6 +0,0 @@
-package org.scanamo
-
-trait PlatformSpecificFormat {
-  implicit def generic[A](implicit A: org.scanamo.generic.Exported[DynamoFormat[A]]): DynamoFormat[A] =
-    A.instance
-}
--- scanamo/src/test/scala-2.x/org/scanamo/generic/FoobarSemiAutoDerivation.scala
+++
@@ -1,9 +0,0 @@
-package org.scanamo.generic
-
-import org.scanamo.DynamoFormat
-import org.scanamo.generic.semiauto.deriveDynamoFormat
-
-case class FoobarSemiAutoDerivation(value: Option[String])
-object FoobarSemiAutoDerivation {
-  implicit val dynamoFormatFoo: DynamoFormat[FoobarSemiAutoDerivation] = deriveDynamoFormat[FoobarSemiAutoDerivation]
-}
