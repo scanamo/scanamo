ec6ea3b9ae891ff99676df8a9075248834dfc6d0
Roberto Tyley <52038+rtyley@users.noreply.github.com>
2024-08-29 22:40:30 +0100

    Scala 3 does far better - but this finally compiles under Scala 2.13

--- scanamo/src/main/scala/org/scanamo/internal/aws/sdkv2/package.scala
+++ scanamo/src/main/scala/org/scanamo/internal/aws/sdkv2/package.scala
@@ -1,16 +1,9 @@
 package org.scanamo.internal.aws
 
-import org.scanamo.internal.aws.sdkv2.HasExpressionAttributes.Foo
 import org.scanamo.request.RequestCondition
 
 package object sdkv2 {
-
-//  implicit class RichBuilder2[T, B <: Moo[T]](builder: B) {
-//    def funk(as: AttributesSummation)(implicit h: HasExpressionAttributes[B]): T =
-//      builder.tableName(as.tableName).attributes(as.attributes).build()
-//  }
-
-  implicit class RichBuilder[B <: Foo](builder: B) {
+  implicit class RichBuilder[B](builder: B) {
     def setOpt[V](opt: Option[V])(f: B => V => B): B = opt.foldLeft(builder) { (b, v) =>
       f(b)(v)
     }
--- scanamo/src/main/scala/org/scanamo/ops/package.scala
+++ scanamo/src/main/scala/org/scanamo/ops/package.scala
@@ -21,19 +21,22 @@ import cats.implicits.*
 import org.scanamo.internal.aws.sdkv2.HasCondition.*
 import org.scanamo.internal.aws.sdkv2.HasExpressionAttributes.*
 import org.scanamo.internal.aws.sdkv2.HasUpdateAndCondition.*
-import org.scanamo.internal.aws.sdkv2.RichBuilder
 import org.scanamo.internal.aws.sdkv2.*
 import org.scanamo.request.*
+import org.scanamo.update.UpdateAndCondition
 import software.amazon.awssdk.services.dynamodb.model
 import software.amazon.awssdk.services.dynamodb.model.*
+import software.amazon.awssdk.utils.builder.SdkBuilder
 
 package object ops {
   type ScanamoOps[A] = Free[ScanamoOpsA, A]
   type ScanamoOpsT[M[_], A] = FreeT[ScanamoOpsA, M, A]
 
-  /** ScanRequest - Option[filterExpression] QueryRequest - keyConditionExpression + Option[filterExpression]
-    * PutItemRequest - Option[conditionExpression] DeleteItemRequest - Option[conditionExpression] UpdateItemRequest -
-    * updateExpression + Option[conditionExpression]
+  /**   - ScanRequest - Option[filterExpression]
+    *   - QueryRequest - keyConditionExpression + Option[filterExpression]
+    *   - PutItemRequest - Option[conditionExpression]
+    *   - DeleteItemRequest - Option[conditionExpression]
+    *   - UpdateItemRequest - updateExpression + Option[conditionExpression]
     *
     * TransactWriteItemsRequest
     *   - Put - Option[conditionExpression]
@@ -46,12 +49,16 @@ package object ops {
     */
   private[ops] object JavaRequests {
 
-    def funk[T, B <: Boom[T, _]](as: AttributesSummation)(builder: B)(implicit
-      h: HasExpressionAttributes[B]
+    def baseSettings[T, B <: SdkBuilder[B, T]](as: AttributesSummation)(builder: B)(implicit
+      h: HasExpressionAttributes[T, B]
     ): T =
-      builder.tableName(as.tableName).attributes(as.attributes).build()
+      new HasExpressionAttributesOps[T, B](builder.set(as.tableName)(h.tableName)).attributes(as.attributes).build()
 
-    def scan(req: ScanamoScanRequest): ScanRequest = funk(req)(
+    def foof[T, B <: SdkBuilder[B, T]](as: AttributesSummation, updateAndCondition: UpdateAndCondition)(builder: B)(
+      implicit h: HasUpdateAndCondition[T, B]
+    ): T = baseSettings[T, B](as)(new HasUpdateAndConditionOps[T, B](builder).updateAndCondition(updateAndCondition))
+
+    def scan(req: ScanamoScanRequest): ScanRequest = baseSettings[ScanRequest, ScanRequest.Builder](req)(
       ScanRequest.builder
         .setOpt(req.index)(_.indexName)
         .consistentRead(req.options.consistent)
@@ -60,7 +67,7 @@ package object ops {
         .setOpt(req.options.filterCondition.map(_.expression))(_.filterExpression)
     )
 
-    def query(req: ScanamoQueryRequest): QueryRequest = funk(req)(
+    def query(req: ScanamoQueryRequest): QueryRequest = baseSettings[QueryRequest, QueryRequest.Builder](req)(
       QueryRequest.builder
         .setOpt(req.index)(_.indexName)
         .consistentRead(req.options.consistent)
@@ -71,32 +78,31 @@ package object ops {
         .keyConditionExpression(req.queryCondition.expression)
     )
 
-    def put(req: ScanamoPutRequest): PutItemRequest = funk(req)(
+    def put(req: ScanamoPutRequest): PutItemRequest = baseSettings[PutItemRequest, PutItemRequest.Builder](req)(
       PutItemRequest.builder
         .item(req.item.asObject.orEmpty.toJavaMap)
         .returnValues(req.ret.asDynamoValue)
         .setOpt(req.condition.map(_.expression))(_.conditionExpression)
     )
 
-    def delete(req: ScanamoDeleteRequest): DeleteItemRequest = funk(req)(
-      DeleteItemRequest.builder
-        .key(req.key.toJavaMap)
-        .returnValues(req.ret.asDynamoValue)
-        .setOpt(req.condition.map(_.expression))(_.conditionExpression)
-    )
+    def delete(req: ScanamoDeleteRequest): DeleteItemRequest =
+      baseSettings[DeleteItemRequest, DeleteItemRequest.Builder](req)(
+        DeleteItemRequest.builder
+          .key(req.key.toJavaMap)
+          .returnValues(req.ret.asDynamoValue)
+          .setOpt(req.condition.map(_.expression))(_.conditionExpression)
+      )
 
-    def update(req: ScanamoUpdateRequest): UpdateItemRequest = funk(req)(
-      UpdateItemRequest.builder
-        .key(req.key.toJavaMap)
-        .returnValues(ReturnValue.ALL_NEW)
-        .updateAndCondition(req.updateAndCondition)
-    )
+    def update(req: ScanamoUpdateRequest): UpdateItemRequest =
+      foof[UpdateItemRequest, UpdateItemRequest.Builder](req, req.updateAndCondition)(
+        UpdateItemRequest.builder.key(req.key.toJavaMap).returnValues(ReturnValue.ALL_NEW)
+      )
 
     def transactItems(req: ScanamoTransactWriteRequest): TransactWriteItemsRequest = {
       val putItems = req.putItems.map { item =>
         TransactWriteItem.builder
           .put(
-            funk[model.Put, model.Put.Builder](item)(
+            baseSettings[model.Put, model.Put.Builder](item)(
               model.Put.builder
                 .item(item.item.asObject.orEmpty.toJavaMap)
                 .setOpt(item.condition.map(_.expression))(_.conditionExpression)
@@ -108,10 +114,8 @@ package object ops {
       val updateItems = req.updateItems.map { item =>
         TransactWriteItem.builder
           .update(
-            funk[model.Update, model.Update.Builder](item)(
-              model.Update.builder
-                .key(item.key.toJavaMap)
-                .updateAndCondition(item.updateAndCondition)
+            foof[model.Update, model.Update.Builder](item, item.updateAndCondition)(
+              model.Update.builder.key(item.key.toJavaMap)
             )
           )
           .build
@@ -119,7 +123,7 @@ package object ops {
       val deleteItems = req.deleteItems.map { item =>
         TransactWriteItem.builder
           .delete(
-            funk[model.Delete, model.Delete.Builder](item)(
+            baseSettings[model.Delete, model.Delete.Builder](item)(
               model.Delete.builder
                 .key(item.key.toJavaMap)
                 .setOpt(item.condition.map(_.expression))(_.conditionExpression)
@@ -131,7 +135,7 @@ package object ops {
       val conditionChecks = req.conditionCheck.map { item =>
         TransactWriteItem.builder
           .conditionCheck(
-            funk[model.ConditionCheck, model.ConditionCheck.Builder](item)(
+            baseSettings[model.ConditionCheck, model.ConditionCheck.Builder](item)(
               model.ConditionCheck.builder
                 .key(item.key.toJavaMap)
                 .conditionExpression(item.condition.expression)
